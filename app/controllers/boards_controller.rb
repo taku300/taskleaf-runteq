1b61c6c2c5ed86081488d515c247a4c3849d0f6f
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-01-21 16:49:40 +0900

    09_create_board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,25 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.build(board_params)
+
+    if @board.save
+      redirect_to boards_path, success: t('defaults.message.created', item: Board.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Board.model_name.human)
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
 end
