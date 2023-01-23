7da8d2399f374a00d8e0d218f486bfc788e7e787
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-01-23 15:56:34 +0900

    12_create_comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :boards, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
@@ -9,4 +10,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
+  def own?(object)
+    id == object.user_id
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   resources :users, only: %i[new create]
-  resources :boards, only: %i[index new create update destroy]
+  resources :boards, only: %i[index new create show] do
+    resources :comments, only: %i[create], shallow: true
+  end
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
